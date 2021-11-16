c5209303dd3b60fd5cfecd786e9dcf00d546dd0d
Winnerforlife <XaHTeR.2000@gmail.com>
2021-11-16 22:24:43 +0100

    Solved the probleb with "class Room", but have a new truble with displaying JS code

---
+++ music_controller/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import fields, serializers
+from . import models
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Room
+        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Room
+        fields = ('guest_can_pause', 'votes_to_skip')
